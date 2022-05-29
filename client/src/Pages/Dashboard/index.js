86fac31b17b7aa06f9b46cef8d4e2168299ee1d5
SudeepMI <sudeepm0599@gmail.com>
2022-05-29 06:52:10 +0545

    ..

--- client/src/Pages/Dashboard/index.js
+++ client/src/Pages/Dashboard/index.js
@@ -67,56 +67,85 @@ function Dashboard() {
   return (
     <div className='container'>
         <div className='row'>
-            <div className='col-4'>
+            {/* <div className='col-4'>
                 <Sidebar user={user} />
-            </div>
-            <div className='col-8'>
-                Your APIS
+            </div> */}
+            <div className='row mt-3'>
+              <div className='col-lg-7'>
+              {api.length > 0 &&  <h3>Your APIS</h3>}
+                <div className='row'>
                {
                      api.map(api  => {
                             return (
-                                <div key={api.id}>
-                                    <h2>{api?.api_id.name}</h2>
-                                    <p>{api?.api_id.description}</p>
-                                    <p>{api?.api_id.endpoint}</p>
-                                    <p>API Key: { api.api_key }</p>
+                                <div className='col-lg-6'>
+                                <div key={api.id} className='card mt-2'>
+                                  <div className='card-header'>
+                                    <h5>{api?.api_id.name}</h5>
+                                  </div>
+                                  <div className='card-body'>
+                                    <code>{api?.api_id.description}</code>
+                                    <p>Endpoint: {api?.api_id.endpoint}</p>
+                                    <pre>API Key: { api.api_key }</pre>
                                     <p>Total calls : { api.call || 0 }</p>
+                                    </div>
+                                </div>
                                 </div>
                             )
                         })
                }
+               </div>
+             
                <h2>All APIs</h2>
+                <div className='row'>
 
 {
                      allApi.map((api,index) => {
                             return (
-                                <div key={index}>
-                                    <h1>{api.name}</h1>
-                                    <p>{api.description}</p>
-                                    <p>{api.endpoint}</p>
-                                    <button onClick={()=>handleUse(api._id)}>use API</button>
+                                <div className='col-lg-6'>
+                                <div key={index} className='card my-4'>
+                                    <div className='card-header'>
+                                    <h5>{api.name}</h5>
+                                    </div>
+                                    <div className='card-body'>
+                                    <code>{api.description}</code>
+                                    <p>Endpoint : {api.endpoint}</p>
+                                    </div>
+                                    <div className='card-footer'>
+                                    <button onClick={()=>handleUse(api._id)} className='btn btn-sm btn-outline-dark'>GET API KEY</button>
+                                    </div>
+                                </div>
                                 </div>
                             )
                         })
                }
-            <div className=''>
-                { !user.isAdmin && <div>
-                    <h2>Contribute API endpoints on ANYSCRAP COMMUNITY</h2>
-                    <div>
-        <input type="text" placeholder="API Name" onChange={e=>setName(e.target.value) } />
-        <textarea placeholder="API Description"
+               </div>
+              </div>
+              <div className='col-lg-5'>
+              <div className='container'>
+                { !user.isAdmin && <div className='col-md-8 m-auto'>
+                    <h5>Contribute API endpoints on ANYSCRAP COMMUNITY</h5>
+                    <div className='form-group'>
+        <input type="text" placeholder="API Name" className='form-control my-3' onChange={e=>setName(e.target.value) } />
+        <textarea 
+        placeholder="Method: GET, POST, PUT, DELETE...."
+        className='form-control my-3'
+        cols={20}
+        rows={10}
         onChange={
           e=>setDescription(e.target.value)
         } />
         <input type="text" placeholder="API Endpoint"
+        className='form-control my-3'
         onChange={
           e=>setEndpoint(e.target.value)
         } />
-        <button onClick={handleSubmit}>Add API</button>
+        <button onClick={handleSubmit} className='btn btn-sm btn-outline-info btn-block'>Add API</button>
       </div>
                     </div>
                 }
-            </div>  
+            </div>
+              </div>
+              
             </div>
         </div>
        
