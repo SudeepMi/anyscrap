1c9752a9f06c75b5e11b340f231f14f7972ab411
SudeepMI <sudeepm0599@gmail.com>
2022-05-29 06:02:05 +0545

    ..

--- server.js
+++ server.js
@@ -54,10 +54,4 @@ if (cluster.isPrimary) {
   console.log(`Worker ${process.pid} started`);
 }
 
-if(process.env.NODE_ENV==="production"){
 
-  app.use(express.static('client/build'));
-  app.get('*',(req, res)=>{
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-  })
-}
