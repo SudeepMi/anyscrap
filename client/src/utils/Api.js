a8ad0354fcc8273d96a73564c04ca8be650fec5c
SudeepMI <sudeepm0599@gmail.com>
2022-05-29 06:11:57 +0545

    ..

--- client/src/utils/Api.js
+++ client/src/utils/Api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
+const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000/api' : 'https://anyscrap.herokuapp.com/api'
 export default  axios.create({
-    baseURL: 'http://localhost:5000/api/',
+    baseURL: baseURL,
         headers: {
             'Access-Control-Allow-Origin': '*',
             'Authorization': 'Bearer ' + localStorage.getItem('token')
